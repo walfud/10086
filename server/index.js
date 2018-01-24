38812b723a449388e09d2ce6b5b70a4c123dad06
walfud <hi@walfud.com>
2018-01-24 12:55:25 +0800

    doc

--- server/index.js
+++ server/index.js
@@ -265,6 +265,7 @@ async function mongo(colName, afn, onSucc, onFail) {
 const proxyBlacklist = new Map()
 async function getProxy() {
     for (let i = 0; i < 100; i++) {
+        // https://github.com/jhao104/proxy_pool
         const proxyServer = await fetch('http://123.207.35.36:5010/get').then(res => res.text()).catch(() => {})
         if (!proxyServer ||
             (proxyBlacklist.has(proxyServer) && proxyBlacklist.get(proxyServer) + 60 * 60 * 1000 > new Date().valueOf())) { // 一小时过期
